b0587788fdffa0c970822ccac5aead5618b18064
Basket134 <arabee.1301@gmail.com>
2026-08-06 23:55:21 +0600

    remove dist from git tracking

--- server/dist/routes/authRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { Login } from "../controllers/authController.js";
-const authRouter = express.Router();
-//authRouter.post('/register', Register)
-authRouter.post('/login', Login);
-export default authRouter;
