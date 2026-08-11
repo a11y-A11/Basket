3b09278302e182128afd835cea97acee59e0b654
Basket134 <arabee.1301@gmail.com>
2026-08-11 16:02:54 +0600

    update all files changes

--- server/routes/deliveryPartnerRoutes.ts
+++ server/routes/deliveryPartnerRoutes.ts
@@ -6,10 +6,10 @@ const deliveryPartnerRouter = express.Router();
 
 deliveryPartnerRouter.post('/login', loginPartner)
 deliveryPartnerRouter.get('/my-delivery', deliveryAuth, getMyDeliveries)
-deliveryPartnerRouter.get('/my-my-delivery/:id', deliveryAuth, getMyDeliveryDetail)
-deliveryPartnerRouter.put('/my-my-delivery/:id/complete', deliveryAuth, completeDelivery)
-deliveryPartnerRouter.put('/my-my-delivery/:id/cancel', deliveryAuth, cancelDelivery)
-deliveryPartnerRouter.put('/my-my-delivery/:id/status', deliveryAuth, updateDeliveryStatus)
-deliveryPartnerRouter.put('/my-my-delivery/:id/location', deliveryAuth, updateLocation)
+deliveryPartnerRouter.get('/my-delivery/:id', deliveryAuth, getMyDeliveryDetail)
+deliveryPartnerRouter.put('/my-delivery/:id/complete', deliveryAuth, completeDelivery)
+deliveryPartnerRouter.put('/my-delivery/:id/cancel', deliveryAuth, cancelDelivery)
+deliveryPartnerRouter.put('/my-delivery/:id/status', deliveryAuth, updateDeliveryStatus)
+deliveryPartnerRouter.put('/my-delivery/:id/location', deliveryAuth, updateLocation)
 
 export default deliveryPartnerRouter;
